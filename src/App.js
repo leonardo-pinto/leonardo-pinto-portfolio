3b43399f44d0f9af13b639e1616b60be42986007
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-08-22 16:23:15 -0300

    Feature: home, about, projects and contacts routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Switch, Route } from 'react-router';
+import { Home, About, Projects, Contact } from './pages';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route path="/about" component={ About } />
+      <Route path="/projects" component={ Projects } />
+      <Route path="/contact" component={ Contact } />
+      <Route exact path="/" component={ Home } />
+    </Switch>
   );
 }
 
