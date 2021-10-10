4fc084de29d88c7220edb7f5e1a23130b854d066
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-10-10 20:33:26 -0300

    update package.json

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import { Switch, Route, HashRouter } from 'react-router-dom';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
@@ -9,7 +9,7 @@ import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <HashRouter basename="/">
+    <BrowserRouter>
       <Switch>
         <Route path="/about" component={About} />
         <Route path="/projects" component={Projects} />
@@ -17,7 +17,7 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route component={NotFound} />
       </Switch>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
