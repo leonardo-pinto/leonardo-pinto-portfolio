903bae491a303e6ce5ef09087b40f70639ebc6cf
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-10-09 17:11:15 -0400

    chore: update imports in App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@
 import React from 'react';
 import Navbar from './components/Nav';
 import ParticlesBackground from './components/ParticlesBackground';
-import Home from './pages/Home';
-import About from './pages/About';
-import Projects from './pages/Projects';
-import Contact from './pages/Contact';
+import Home from './components/Home';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
 import Footer from './components/Footer';
 
 function App() {
