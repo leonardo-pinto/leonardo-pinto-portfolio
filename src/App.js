7e58ac3c16053c7c18447fa5b041ab262a5898af
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-10-09 16:44:07 -0400

    refactor: components render in single page instead of multiple routes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import Navbar from './components/Nav';
 import ParticlesBackground from './components/ParticlesBackground';
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
-import NotFound from './pages/NotFound';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <ParticlesBackground />
       <Navbar />
-      <Switch>
-        <Route path="/about" component={About} />
-        <Route path="/projects" component={Projects} />
-        <Route path="/contact" component={Contact} />
-        <Route exact path="/" component={Home} />
-        <Route component={NotFound} />
-      </Switch>
-    </BrowserRouter>
+      <Home />
+      <About />
+      <Projects />
+      <Contact />
+      <Footer />
+    </>
   );
 }
 
